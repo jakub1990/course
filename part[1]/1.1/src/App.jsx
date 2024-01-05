65d8492521860aeb70e2fe061ff96c655da7f98d
Kuba <jakub.rozanski@4real.pl>
2024-01-05 20:44:39 +0100

    finish javascript

--- part[1]/1.1/src/App.jsx
+++ part[1]/1.1/src/App.jsx
@@ -3,21 +3,31 @@ import Content from './Content.jsx';
 import Total from './Total.jsx';
 
 function App() {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+  name: 'Half Stack application development'
+}
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
-      <Total exercises1={10} exercises2={7} exercises3={14} />
+      <Header course={course.name}/>
+      <Content part={parts[0].name} exercises={parts[0].exercises} />
+      <Content part={parts[1].name} exercises={parts[1].exercises} />
+      <Content part={parts[2].name} exercises={parts[2].exercises} />
+      <Total exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
     </div>
   )
 }
